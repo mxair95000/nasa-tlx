6b9df386465d5f48bbdec463230b1cf3b2f63ec3
CH2J <44470696+mxair95000@users.noreply.github.com>
2021-10-04 22:47:29 +0800

    Update default.js

--- setup/js/default.js
+++ setup/js/default.js
@@ -59,12 +59,12 @@ $(document).ready(function() {
 		pairs_length,
 		final_result = [],
 		demands = [
-			["md", "Mental demand 腦力負荷"],
-			["pd", "Physical demand"],
-			["td", "Temporal demand"],
-			["op", "Performance"],
-			["ef", "Effort"],
-			["fr", "Frustration"]
+			["md", "Mental demand 腦力需求"],
+			["pd", "Physical demand 體力需求"],
+			["td", "Temporal demand 時間需求"],
+			["op", "Performance 績效表現"],
+			["ef", "Effort 付出努力"],
+			["fr", "Frustration 挫折感"]
 		],
 		tableoutput = "",
 		no_score = "–";
